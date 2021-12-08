a2c6ccbb29d8c815ea9e109d9eabaa980ad9022f
Casey <caseyjtite@gmail.com>
2021-12-08 12:19:51 -0800

    iterating arrays

---
+++ packing-list.js
@@ -0,0 +1,17 @@
+const packingList = [
+  "bowls",
+  "plates",
+  "pots",
+  "pans",
+  "eating utensils",
+  "glasses",
+  "cups",
+  "cooking utensils",
+];
+
+console.log("Kitchen stuff to pack:");
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
+
+console.log(packingList[3]); // returns pans arrrays start at index[0]
