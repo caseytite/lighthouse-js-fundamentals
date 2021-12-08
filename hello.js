9aa347873d8f01ca8a072b590e81b064f21e9dda
Casey <caseyjtite@gmail.com>
2021-12-07 20:36:30 -0800

    return and cl dif

--- hello.js
+++ hello.js
@@ -1,5 +1,18 @@
 const sayHello = function (name) {
-  console.log("Hello " + name);
+  console.log("Hello, " + name);
 };
 
 sayHello("Casey");
+
+const sayHelloToConsole = function (name) {
+  console.log("Hello, " + name);
+};
+
+sayHelloToConsole("Casey");
+
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+};
+
+const greeting = returnSayHello("Casey");
+console.log(greeting);
