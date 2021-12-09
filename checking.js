7a6612a7e618e18e832fac1cfe66432b73a7b350
Casey <caseyjtite@gmail.com>
2021-12-08 16:22:47 -0800

    checking to see

--- checking.js
+++ checking.js
@@ -1,1 +1,11 @@
-console.log("hello");
+const range = function (start, end, step) {
+  let newArray = [];
+  for (let i = start; i <= end; i = i + step) {
+    newArray.push(i);
+  }
+  return newArray;
+};
+
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
